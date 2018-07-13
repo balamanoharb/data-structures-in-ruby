af4be86a4e2af49f98fe8b2000bdbb2484ffb7a5
balamanoharb <balamanoharb@gmail.com>
2018-07-13 02:27:32 +0000

    Initial Setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,16 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "data_store"
+require "simplecov"
+require "codecov"
 
-require "minitest/autorun"
-require 'simplecov'
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
+  [
+    # SimpleCov::Formatter::Console,
+    # Want a nice code coverage website? Uncomment this next line!
+    SimpleCov::Formatter::HTMLFormatter
+    SimpleCov::Formatter::Codecov
+  ]
+)
 SimpleCov.start
 
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
+require "minitest/autorun"
